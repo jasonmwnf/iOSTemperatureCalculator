e50a6c1ea19204fa42463f044d4eac2ea1bcda32
Jason Williams <jasonmw_nf@hotmail.com>
2016-01-19 02:58:51 -0330

    Initial Commit

---
+++ TemperatureCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TemperatureCalculator
+//
+//  Created by Jason Williams on 2016-01-19.
+//  Copyright © 2016 Screaming Goat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TemperatureCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TemperatureCalculator
+//
+//  Created by Jason Williams on 2016-01-19.
+//  Copyright © 2016 Screaming Goat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
